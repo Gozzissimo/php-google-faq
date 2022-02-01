08e06a2766f1cdb550fae17966f41f681511bc0d
Gozzissimo <fantini.nico@gmail.com>
2022-02-01 23:06:52 +0100

    bozza header e main

--- index.php
+++ index.php
@@ -63,21 +63,38 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Google Faq</title>
 </head>
 <body>
-    <header></header>
+    <header>
+        <div class="header-top flex">
+            <div class="title flex">
+                <img src="img/logo.png" alt="logo">
+                <h1>Privacy e Termini</h1>
+            </div>
+            <div class="account flex">
+                <i class="fas fa-th"></i>
+                <div>
+                    N
+                </div>
+            </div>
+        </div>
+        <div class="header-bottom"></div>
+    </header>
     <main>
-        <?php
-            foreach ($faq as $key => $value) {
-                ?>
-                    <h2 class="question" ><?php echo($value["question"]); ?></h2>
-                <?php
-                ?>
-                    <p class="answer"><?php echo($value["answer"]); ?></p>
-                <?php
-            }
-        ?>
+        <div class="block">
+            <?php
+                foreach ($faq as $key => $value) {
+                    ?>
+                        <h2 class="question" ><?php echo($value["question"]); ?></h2>
+                    <?php
+                    ?>
+                        <p class="answer"><?php echo($value["answer"]); ?></p>
+                    <?php
+                }
+            ?>
+        </div>
     </main>
     <footer></footer>
 </body>
